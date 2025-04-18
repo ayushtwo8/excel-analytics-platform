545ac2412c252014ea95e4152a8a66dff7cf7d23
Ayush Tiwari <ayushh8866@gmail.com>
2025-04-18 22:18:10 +0530

    init

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('express');
+})
+
+app.listen(5000, () => {
+    console.log('App is running on port 5000');
+})
