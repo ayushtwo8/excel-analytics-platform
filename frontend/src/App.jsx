21cf610a02d2bb1058dd3c5927c45abe13d09306
khushi28p <162727674+khushi28p@users.noreply.github.com>
2025-05-02 16:59:11 +0530

    shadcn setup complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
-function App() {
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Home from './components/Home'
+
+const App = () => {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
