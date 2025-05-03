a34996a8b704015b3c35a4ed1f7efd3ed2a5bd0f
khushi28p <162727674+khushi28p@users.noreply.github.com>
2025-05-03 17:43:06 +0530

    login added

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,85 @@
-import React from 'react'
+import React from "react";
+import { LuChartNoAxesCombined } from "react-icons/lu";
+import { FcGoogle } from "react-icons/fc";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
 const Login = () => {
   return (
     <>
-      <h1>Login page</h1>
+      <div className="grid min-h-svh lg:grid-cols-2 ">
+        <div className="flex flex-col gap-4 p-6 md:p-10">
+          <div className="flex justify-center gap-2 md:justify-start">
+            <a href="#" className="flex justify-center items-center gap-2 text-green-800 font-bold text-2xl">
+              <LuChartNoAxesCombined />
+              Excelytics
+            </a>
+          </div>
+          <div className="flex flex-1 items-center justify-center">
+            <div className="w-full max-w-sm">
+              <form className='flex flex-col gap-6  border p-6 rounded-lg shadow-md '>
+                <div className="flex flex-col items-center gap-2 text-center">
+                  <h1 className="text-2xl font-bold">Login to your account</h1>
+                  <p className="text-balance text-sm text-muted-foreground">
+                    Enter your email below to login to your account
+                  </p>
+                </div>
+                <div className="grid gap-6">
+                  <div className="grid gap-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="john@example.com"
+                      required
+                    />
+                  </div>
+                  <div className="grid gap-2">
+                    <div className="flex items-center">
+                      <Label htmlFor="password">Password</Label>
+                      <a
+                        href="#"
+                        className="ml-auto text-xs underline-offset-4 hover:underline"
+                      >
+                        Forgot your password?
+                      </a>
+                    </div>
+                    <Input id="password" type="password" placeholder='********' required />
+                  </div>
+                  <Button type="submit" className="w-full bg-green-800 hover:bg-green-700">
+                    Login
+                  </Button>
+                  <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                    <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                      Or continue with
+                    </span>
+                  </div>
+                  <Button variant="outline" className="w-full">
+                  <FcGoogle />
+                    Login with Google
+                  </Button>
+                </div>
+                <div className="text-center text-sm">
+                  Don&apos;t have an account?{" "}
+                  <a href="#" className="underline underline-offset-4 text-blue-700">
+                    Sign up
+                  </a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="hidden lg:flex align-center items-center justify-start ">
+          <img
+            src="/login-illustration.jpg"
+            alt="Image"
+            className="w-4/5 h-auto object-contain"
+          />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,25 +7,25 @@ const Navbar = () => {
     <>
       <div className="w-full py-5 px-12 border-b border-gray-300/40">
         <nav className="flex justify-between items-center align-center">
-          <div className="flex justify-center items-center gap-2 text-green-700 font-bold text-2xl">
+          <div className="flex justify-center items-center gap-2 text-green-800 font-bold text-2xl">
             <LuChartNoAxesCombined size={25} />
             <div>Excelytics</div>
           </div>
 
           <ul className="flex items-center gap-5">
-            <li className="text-sm px-4 py-1 hover:border-b-2 border-green-700 cursor-pointer">
+            <li className="text-sm px-4 py-1 hover:border-b-2 border-green-800 cursor-pointer">
               Home
             </li>
-            <li className="text-sm px-4 py-1 hover:border-b-2 border-green-700 cursor-pointer">
+            <li className="text-sm px-4 py-1 hover:border-b-2 border-green-800 cursor-pointer">
               Dashboard
             </li>
-            <li className="text-sm px-4 py-1 hover:border-b-2 border-green-700 cursor-pointer">
+            <li className="text-sm px-4 py-1 hover:border-b-2 border-green-800 cursor-pointer">
               About
             </li>
-            <li className="text-sm px-4 py-1 hover:border-b-2 border-green-700 cursor-pointer">
+            <li className="text-sm px-4 py-1 hover:border-b-2 border-green-800 cursor-pointer">
               Contact
             </li>
-            <Button className="bg-green-700 hover:bg-green-800 cursor-pointer">
+            <Button className="bg-green-800 hover:bg-green-700 cursor-pointer">
               Login
             </Button>
           </ul>
